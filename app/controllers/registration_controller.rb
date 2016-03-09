88c7dde21bf05adfcf6e2cf7553e8326e65241b6
nat14 <naryucha14@gmail.com>
2016-03-09 11:47:23 -0800

    Added password checker functionality, more tests, and bootstrap to site

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -5,19 +5,18 @@ class RegistrationController < ApplicationController
 
   def register_user
     if params.has_key?(:username) && !params[:username].strip.empty?
-      # TODO: check for duplicate username
-
+      # check duplicate username
       user = User.find_by_username(params[:username])
-      if user.nil?
+      if user.nil? && validatePassword(params[:password])
         user1 = User.new
         # add user input to user1 variable
         user1.firstname = params[:firstname] unless params[:firstname].empty?
         user1.lastname = params[:lastname] unless params[:lastname].empty?
         user1.streetaddr = params[:streetaddr] unless params[:streetaddr].empty?
         user1.city = params[:city] unless params[:city].empty?
-        user1.city = params[:state] unless params[:state].empty?
-        user1.city = params[:zipcode] unless params[:zipcode].empty?
-        user1.city = params[:country] unless params[:country].empty?
+        user1.state = params[:state] unless params[:state].empty?
+        user1.zipcode = params[:zipcode] unless params[:zipcode].empty?
+        user1.country = params[:country] unless params[:country].empty?
         user1.username = params[:username]
         user1.password = params[:password]
         user1.save
@@ -26,11 +25,14 @@ class RegistrationController < ApplicationController
         add_phone(user1, params[:phone2])
         add_phone(user1, params[:phone3])
         # save userid to cookies for tracking user session
+
         cookies[:userid] = user1.id
         redirect_to '/registration/user_info'
       else
-        redirect_to '/registration/register'
-        flash[:notice] = "User ID taken, try another."
+        if !user.nil?
+          flash[:notice] = "User ID taken, try another."
+        end
+        render '/registration/register'
       end
     else
       # stay on the same page if no username input
@@ -80,5 +82,42 @@ class RegistrationController < ApplicationController
     cookies.delete :userid
     flash[:notice] = "You are now logged out"
     redirect_to '/registration/login'
+  end
+
+  # main method to check password and username
+  def validatePassword(password)
+    if check6char(password) || !pswdDoesNotContainSpecial(password) || pswdContainPswd(password)
+      return false
+    else
+      return true
+    end
+  end
+
+  # method that checks if the password are more than 6 characters
+  def check6char(str)
+    if str.length < 6
+      flash[:alert] =  "Password must have more than 6 characters"
+      true
+    end
+  end
+
+  # checks the password for special characters. returns true if they are present
+  def pswdDoesNotContainSpecial(str)
+    a = (str =~ /[!#$]/)
+    if a.to_i > 0
+      true
+    else
+      flash[:alert] = "Password must contain at least one $, !, or #."
+      false
+    end
+  end
+
+  def pswdContainPswd(str)
+    if str.upcase.include? "PASSWORD"
+      flash[:alert] = "Password can not contain password!"
+      true
+    else
+      false
+    end
   end
 end
--- spec/features/register_users_spec.rb
+++ spec/features/register_users_spec.rb
@@ -2,16 +2,47 @@ require 'rails_helper'
 
 RSpec.feature "RegisterUsers", type: :feature do
   it "can create a user and store their information in the database" do
-    visit '/registration/register'
-
+    visit '/'
     fill_reg_page
     expect(page).to have_text("Shane")
     expect(page).to have_text("McCorkle")
-    expect(page).to have_text("user1")
-    expect(page).to have_text("pass1")
+    expect(page).to have_text("User111")
     expect(page).to have_text("123-456-7890")
     expect(page).to have_text("No Phone")
+  end
 
+  it "can verify that a username is already in use and not create another user with same username" do
+    visit '/'
+    page.fill_in 'User Name', with: 'User111'
+    page.fill_in 'Password', with: 'pass1!!!'
+    click_on 'Register'
+    click_on 'Log Out'
+    # save_and_open_page
+    click_on 'Register'
+    fill_reg_page
+    expect(page).to have_text("User ID taken, try another.")
+  end
+
+  it "validate password" do
+    visit '/'
+    page.fill_in 'User Name', with: 'User111'
+    page.fill_in 'Password', with: 'pass1'
+    click_on 'Register'
+    expect(page).to have_text("Password must have more than 6 characters")
+  end
+
+  it "can log in" do
+    visit '/'
+    fill_reg_page
+    click_on 'Login'
+    page.fill_in 'User Name', with: 'User111'
+    page.fill_in 'Password', with: 'pass1!!!'
+    click_on 'Login'
+    expect(page).to have_text("Shane")
+    expect(page).to have_text("McCorkle")
+    expect(page).to have_text("User111")
+    expect(page).to have_text("123-456-7890")
+    expect(page).to have_text("No Phone")
   end
 
   def fill_reg_page
@@ -22,9 +53,10 @@ RSpec.feature "RegisterUsers", type: :feature do
     page.fill_in 'State', with: 'CA'
     page.fill_in 'Zip Code', with: '92130'
     page.fill_in 'Country', with: 'US'
-    page.fill_in 'User Name', with: 'user1'
-    page.fill_in 'Password', with: 'pass1'
+    page.fill_in 'User Name', with: 'User111'
+    page.fill_in 'Password', with: 'pass1!!!'
     page.fill_in 'Phone Number 1', with: '123-456-7890'
     click_button 'Register'
   end
+
 end

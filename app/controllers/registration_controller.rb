7ae705e7c909bf829531f07a6f5f27d4b4183d0c
nat14 <naryucha14@gmail.com>
2016-03-08 16:23:55 -0800

    Added functionality for login, logout, empty fields being saved as NULL instead of an empty string.

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -6,23 +6,35 @@ class RegistrationController < ApplicationController
   def register_user
     if params.has_key?(:username) && !params[:username].strip.empty?
       # TODO: check for duplicate username
-      user1 = User.new
-      # add user input to user1 variable
-      user1.firstname = params[:firstname]
-      user1.lastname = params[:lastname]
-      user1.username = params[:username]
-      user1.password = params[:password]
-      user1.save
-      # add phone to user
-      add_phone(user1, params[:phone1])
-      add_phone(user1, params[:phone2])
-      add_phone(user1, params[:phone3])
-      # save userid to cookies for tracking user session
-      cookies[:userid] = user1.id
-      redirect_to '/registration/user_info'
+
+      user = User.find_by_username(params[:username])
+      if user.nil?
+        user1 = User.new
+        # add user input to user1 variable
+        user1.firstname = params[:firstname] unless params[:firstname].empty?
+        user1.lastname = params[:lastname] unless params[:lastname].empty?
+        user1.streetaddr = params[:streetaddr] unless params[:streetaddr].empty?
+        user1.city = params[:city] unless params[:city].empty?
+        user1.city = params[:state] unless params[:state].empty?
+        user1.city = params[:zipcode] unless params[:zipcode].empty?
+        user1.city = params[:country] unless params[:country].empty?
+        user1.username = params[:username]
+        user1.password = params[:password]
+        user1.save
+        # add phone to user
+        add_phone(user1, params[:phone1])
+        add_phone(user1, params[:phone2])
+        add_phone(user1, params[:phone3])
+        # save userid to cookies for tracking user session
+        cookies[:userid] = user1.id
+        redirect_to '/registration/user_info'
+      else
+        redirect_to '/registration/register'
+        flash[:notice] = "User ID taken, try another."
+      end
     else
       # stay on the same page if no username input
-      render '/registration/register'
+        render '/registration/register'
     end
   end
 
@@ -37,6 +49,9 @@ class RegistrationController < ApplicationController
     # get user record using userid from cookies
     if !cookies[:userid].nil?
       @user = User.find(cookies[:userid])
+    else
+      flash[:notice] = "Please Log In to view User Info"
+      redirect_to '/registration/login'
     end
   end
 
@@ -47,16 +62,23 @@ class RegistrationController < ApplicationController
     if params.has_key?(:username) && !params[:username].strip.empty? && params.has_key?(:password) && !params[:password].strip.empty?
       user = User.find_by_username(params[:username])
       if user.nil?
-        flash[:alert] = "Wrong Username"
+        flash[:alert] = "Log in failed, try again; Wrong Username"
         render 'login'
       else
         if user.password == params[:password]
           cookies[:userid] = user.id
           redirect_to '/registration/user_info'
         else
-          flash[:alert] = "Wrong Password"
+          render 'login'
+          flash[:alert] = "Log in failed, try again; Wrong Password"
         end
       end
     end
+  end
+
+  def logout
+    cookies.delete :userid
+    flash[:notice] = "You are now logged out"
+    redirect_to '/registration/login'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'registration/logout'
+
   post 'registration/login_user'
 
   get 'registration/login'

a534843499c3515c991aa736c57af967301695c6
Shane McCorkle <hello@shanemccorkle.com>
2016-03-10 10:48:48 -0800

    Deleted two of the migration files because they were causing errors in the rake db:migrate

--- db/migrate/20160307211802_remove_fullname_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFullnameFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :fullname, :string
-  end
-end
--- db/migrate/20160307211924_add_details_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :firstname, :string
-    add_column :users, :lastname, :string
-  end
-end
